5f79f08bedce0b83b0e837a8abb664145d73a891
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-21 12:15:12 +0100

    initialised the product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ProductModule],
 })
 export class AppModule {}

d47d448840fdc171395aa3f436fe69c42316b1e3
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-21 12:16:24 +0100

    initialised the order module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductModule],
+  imports: [AuthModule, UserModule, ProductModule, OrderModule],
 })
 export class AppModule {}

1f610886c26e05b9bbc9123710dc20e4241bbe05
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-21 12:14:24 +0100

    setted up the auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [UsersModule],
+  imports: [AuthModule],
 })
 export class AppModule {}
--- src/roles/roles.decorator.ts
+++
@@ -1,2 +0,0 @@
-import { Reflector } from '@nestjs/core';
-export const Roles = Reflector.createDecorator<string[]>();

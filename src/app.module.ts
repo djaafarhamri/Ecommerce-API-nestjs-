181cf6d5b2427f0ceaac99c1f86ca1d417693d9e
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-21 12:16:42 +0100

    initialised the cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductModule, OrderModule],
+  imports: [AuthModule, UserModule, ProductModule, OrderModule, CartModule],
 })
 export class AppModule {}

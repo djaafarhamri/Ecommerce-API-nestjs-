931e0bf3d1e3fece4e7a2304b0261aae3dd89531
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-26 16:47:11 +0100

    made get All users route guarde by adminguard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Put, UseGuards, Body, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from '../auth/guard';
+import { AdminGuard } from 'src/auth/guard/admin.guard';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +13,7 @@ export class UserController {
     return this.userService.me(req);
   }
 
+  @UseGuards(JwtGuard, AdminGuard)
   @Get('all')
   findAll() {
     return this.userService.findAll();

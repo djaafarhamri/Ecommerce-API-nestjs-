231cc9001b41aebba7e2f9619f9fddb9f491497b
djaafarhamri <hamridjaafar12@gmail.com>
2023-09-27 15:28:13 +0100

    added some dtos

--- prisma/migrations/20230927142140_uirfr/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `price` on the `CartItem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "CartItem" DROP COLUMN "price";
